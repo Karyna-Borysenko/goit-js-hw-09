0075428370e16854a5306074071c159b9bb8d508
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-24 15:27:51 +0300

    01-color-switcher-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+buttonStart.addEventListener('click', onbuttonStartClick);
+buttonStop.addEventListener('click', onbuttonStopClick);
+
+function onbuttonStartClick() {
+  buttonStart.setAttribute('disabled', 'disabled');
+  buttonStop.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onbuttonStopClick() {
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
